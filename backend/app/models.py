014fbaf2cc73e35681d38bb807209e866f96771b
Mike Risse <rissem@gmail.com>
2025-04-17 10:52:50 -0700

    migration test, add alternate description to items

--- backend/app/models.py
+++ backend/app/models.py
@@ -25,7 +25,8 @@ class UserRegister(SQLModel):
 
 # Properties to receive via API on update, all are optional
 class UserUpdate(UserBase):
-    email: EmailStr | None = Field(default=None, max_length=255)  # type: ignore
+    email: EmailStr | None = Field(
+        default=None, max_length=255)  # type: ignore
     password: str | None = Field(default=None, min_length=8, max_length=40)
 
 
@@ -43,7 +44,8 @@ class UpdatePassword(SQLModel):
 class User(UserBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     hashed_password: str
-    items: list["Item"] = Relationship(back_populates="owner", cascade_delete=True)
+    items: list["Item"] = Relationship(
+        back_populates="owner", cascade_delete=True)
 
 
 # Properties to return via API, id is always required
@@ -60,6 +62,7 @@ class UsersPublic(SQLModel):
 class ItemBase(SQLModel):
     title: str = Field(min_length=1, max_length=255)
     description: str | None = Field(default=None, max_length=255)
+    alternate_description: str | None = Field(default=None, max_length=255)
 
 
 # Properties to receive on item creation
@@ -69,7 +72,8 @@ class ItemCreate(ItemBase):
 
 # Properties to receive on item update
 class ItemUpdate(ItemBase):
-    title: str | None = Field(default=None, min_length=1, max_length=255)  # type: ignore
+    title: str | None = Field(
+        default=None, min_length=1, max_length=255)  # type: ignore
 
 
 # Database model, database table inferred from class name
